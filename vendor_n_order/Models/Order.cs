a469e33b471cf8f0e925a3edb224d2712cae1bd0
Harold_Mesa <harold.mesa@icloud.com>
2022-09-30 12:08:32 -0500

    Pass 3rd TDD for Order Class.

--- vendor_n_order/Models/Order.cs
+++ vendor_n_order/Models/Order.cs
@@ -4,11 +4,13 @@ namespace Business.Models
   {
     //Properties
     public string Title { get; set; }
+    public string Description { get; set; }
     
     //Constructor
     public Order(string title, string description, int price, string date)
     {
       Title = title;
+      Description = description;
     }
     //Methods
   }
