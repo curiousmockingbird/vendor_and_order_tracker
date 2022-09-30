ac7d25e1ad33fc8e81543f578b9424a62061fb0b
Harold_Mesa <harold.mesa@icloud.com>
2022-09-30 11:59:19 -0500

    Add 2nd TDD. Good fail.

--- vendor_n_order.Tests/ModelTests/OrderTests.cs
+++ vendor_n_order.Tests/ModelTests/OrderTests.cs
@@ -16,5 +16,16 @@ namespace Business.Tests
       //Assert
       Assert.AreEqual(typeof(Order), order1.GetType());
     }
+
+    [TestMethod]
+    public void SetOrderTitle_SetsOrderTitle_String()
+    {
+      //Arrange
+      Order order1 = new Order("Matt's order", "Cookies and pastries", 30, "Sep 20, 2022");
+      //Act
+      string result = order1.Title;
+      //Assert
+      Assert.AreEqual("Matt's order", result);
+    }
   }
 }
